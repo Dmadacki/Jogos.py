2438fa102f8dabfec9b03836a256ea1e5e5e0feb
Dmadacki <107811456+Dmadacki@users.noreply.github.com>
2023-01-19 16:22:20 -0300

    iniciando programa

--- forca.py
+++ forca.py
@@ -1,1 +1,13 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+    palavra_secreta = "banana"
+
+    while (nao enforcou E nao acertou):
+    print ("Fim do jogo")
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
 
